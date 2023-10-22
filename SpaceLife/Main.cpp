8a490811224a00ccadca046e1731f7477d26387a
YO-U <enoxy7@gmail.com>
2023-10-22 17:02:46 +0200

    Palindrome Number
    https://leetcode.com/problems/palindrome-number

--- SpaceLife/Main.cpp
+++ SpaceLife/Main.cpp
@@ -3,103 +3,21 @@
 using namespace std;
 class Solution {
 public:
-    bool isValid(string s) 
-    {
-        bool answer = true;
-        int backNum;
-        bool checkStart,checkEnd = false;
-
-        
-
-        while(s != "")
+    bool isPalindrome(int x) {
+        string num = to_string(x);
+        int endNum = num.length();
+        bool answer = false;
+        endNum--;
+        for (int i = 0; i < num.length(); i++)
         {
-            for (int i = 0; i < s.length();)
-            {
-                for (int j = 0; j < s.length(); j++)
-                {
-                    if (s[j] == ')' || s[j] == ']' || s[j] == '}') {
-                        checkEnd = true;
-                        break;
-                    }
-                    else
-                    {
-                        checkEnd = false;
-                    }
-                }
-                for (int g = 0; g < s.length(); g++)
-                {
-
-                    if (s[g] == '(' || s[g] == '[' || s[g] == '{') {
-                        checkStart = true;
-                        break;
-                    }
-                    else
-                    {
-                        checkStart = false;
-                    }
-                }
-                if( s[0] != ')' && s[0] != ']' && s[0] != '}' && checkEnd && checkStart )
-                {
-                    backNum = i - 1;
-                    switch (s[i])
-                    {
-                    case ')':
-                        if (s[backNum] == '(') 
-                        {
-                            s.erase(i,1);
-                            s.erase(backNum,1);
-                            i = 0;
-                            break;
-                        }
-                        else
-                        {
-                            answer = false;
-                            s = "";
-                            break;
-                        }
-                        break;
-                    case ']':
-                        if (s[backNum] == '[') 
-                        {
-                            s.erase(i, 1);
-                            s.erase(backNum, 1);
-                            i = 0;
-                            break;
-                        }
-                        else
-                        {
-                            answer = false;
-                            s = "";
-                            break;
-                        }
-                        break;
-                    case '}':
-                        if (s[backNum] == '{') 
-                        {
-                            s.erase(i, 1);
-                            s.erase(backNum, 1);
-                            i = 0;
-                            break;
-
-                        }
-                        else
-                        {
-                            answer = false;
-                            s = "";
-                            break;
-                        }
-                        break;
-                    default:
-                        i++;
-                        break;
-                    }
-                }
-                else {
-                    answer = false;
-                    s = "";
-                    break;
-                }
+            if (num[i] != num[endNum]) {
+                answer = false;
+                break;
             }
+            else {
+                answer = true;
+            }
+            endNum--;
         }
         cout << answer;
         return answer;
@@ -107,6 +25,6 @@ public:
 };
 int main() {
     Solution a;
-    a.isValid("({[(([[()]]))]})");
+    a.isPalindrome(1234321);
     return 0;
 }
